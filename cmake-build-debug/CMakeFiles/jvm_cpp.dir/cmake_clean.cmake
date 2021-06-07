c4a37c8db368ece6189bcd61fa26a57b919f9110
zhengzp <727851584@qq.com>
2021-06-07 11:09:53 +0800

    初始化项目

---
+++ cmake-build-debug/CMakeFiles/jvm_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jvm_cpp.dir/main.cpp.o"
+  "jvm_cpp"
+  "jvm_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jvm_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
